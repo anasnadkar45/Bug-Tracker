fa05a82d56d5b7fb594f423bf0d253931fa61211
anasnadkar45 <anasnadkar23@gmail.com>
2024-10-28 12:20:05 +0530

    Project Completed

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    staticPageGenerationTimeout: 60,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/mytasks',
+        destination: '/api/fallback', // Fallback route if `/mytasks` fails to build
+      },
+    ];
+  },
 };
 
 export default nextConfig;
